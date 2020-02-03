f5bb8067b6f7f7c71beb329501c40a19aa86259f
Adam Alenius <adam.alenius@gmail.com>
2020-02-03 10:35:50 +0100

    feat: add unique prop to cuisine model

--- models/CuisineModel.js
+++ models/CuisineModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 var CuisineSchema = new mongoose.Schema({
-  cuisine: String
+  cuisine: { type: String, unique: true }
 });
 
 var Cuisine = mongoose.model("Cuisine", CuisineSchema);
